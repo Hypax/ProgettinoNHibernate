4070e34bf5a4f3287ebac82b0b84cd580bf7c587
Riccardo Galli <riccardo.galli@scai-consulting.grupposcai.it>
2016-02-26 17:12:00 +0100

    Created Project

---
+++ ProgettinoNHibernate/ProgettinoNHibernate/ProgettinoNHibernate/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(ProgettinoNHibernate.Startup))]
+namespace ProgettinoNHibernate
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
